d3f65fedc990298b07f21f0a2d1a384dcdc2b0c7
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-09 21:42:55 -0800

    login register

--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,20 @@ router.get("/login", (req, res) => {
     isLogin: true,
   });
 });
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 router.get("/register", (req, res) => {
   res.render("register", {
     title: "Register | Otabek",
     isRegister: true,
   });
+});
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 export default router;
