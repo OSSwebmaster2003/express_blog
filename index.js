b05941cd04f437609d0450bae853044ac0b635d2
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-09 22:41:34 -0800

    mongo db connected

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { create } from "express-handlebars";
+import mongoose from "mongoose";
 import AuthRoutes from "./routes/auth.js";
 import ProductRoutes from "./routes/products.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,11 +20,25 @@ app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
 app.use(AuthRoutes);
 app.use(ProductRoutes);
 
-const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => {
-  console.log(`Server running on Port : ${PORT}`);
-});
+const startApp = () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    });
+
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () => {
+      console.log(`Server running on Port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
